85f8506e18afaac962212ee12c482ad2cf6542c3
briezielke <157852377+briezielke@users.noreply.github.com>
2024-01-26 16:58:38 -0600

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
 let dog = sprites.create(assets.image`puppy`, SpriteKind.Player)
 scene.setBackgroundColor(3)
 controller.moveSprite(dog)
+let mySprite = sprites.create(assets.image`bone`, SpriteKind.Player)
