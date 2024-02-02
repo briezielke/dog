71de9757ebc812bbcff2d44f38e84f2030c650c1
briezielke <157852377+briezielke@users.noreply.github.com>
2024-02-01 20:26:40 -0600

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,16 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    mySprite.setPosition(randint(0, scene.screenWidth()), randint(0, scene.screenHeight()))
+    music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.UntilDone)
+})
+info.onScore(5, function () {
+    info.changeCountdownBy(30)
+})
+let mySprite: Sprite = null
 let dog = sprites.create(assets.image`puppy`, SpriteKind.Player)
 scene.setBackgroundColor(3)
 controller.moveSprite(dog)
-let mySprite = sprites.create(assets.image`bone`, SpriteKind.Player)
+mySprite = sprites.create(assets.image`bone`, SpriteKind.Food)
+mySprite.setPosition(randint(0, scene.screenWidth()), randint(0, scene.screenHeight()))
+tiles.setCurrentTilemap(tilemap`level1`)
+info.startCountdown(60)
