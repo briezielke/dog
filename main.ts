73f5727a4f49d2c53c7f468db82a4727c4017af9
briezielke <157852377+briezielke@users.noreply.github.com>
2024-02-15 20:07:19 -0600

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,21 @@
+info.onScore(50, function () {
+    game.setGameOverEffect(true, effects.hearts)
+    game.gameOver(true)
+})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     info.changeScoreBy(1)
     mySprite.setPosition(randint(0, scene.screenWidth()), randint(0, scene.screenHeight()))
+    tiles.placeOnRandomTile(otherSprite, assets.tile`transparency16`)
     music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.UntilDone)
 })
-info.onScore(5, function () {
-    info.changeCountdownBy(30)
+info.onScore(20, function () {
+    info.changeCountdownBy(60)
+    tiles.setCurrentTilemap(tilemap`level2`)
+    scene.cameraFollowSprite(dog)
 })
 let mySprite: Sprite = null
-let dog = sprites.create(assets.image`puppy`, SpriteKind.Player)
+let dog: Sprite = null
+dog = sprites.create(assets.image`puppy`, SpriteKind.Player)
 scene.setBackgroundColor(3)
 controller.moveSprite(dog)
 mySprite = sprites.create(assets.image`bone`, SpriteKind.Food)
